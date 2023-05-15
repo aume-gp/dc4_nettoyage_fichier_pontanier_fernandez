720efcb789ef8a74ce9c244728cb9fa73ea01372
Guillaume Pontanier <guillaume.pontanier@gmail.com>
2023-05-15 16:58:48 +0200

    Suppression lignes vides et ajout id auto incrémenté

--- fichier.py
+++ fichier.py
@@ -11,5 +11,12 @@ df = df[['Period', 'Data_value', 'Series_title_2']]
 #Consigne n°3 : Filtrer les résultats en ne gardant que les résultats avec Credit, Debit et Services dans la colonne Series_title_2
 df = df.query("Series_title_2 in ('Credit','Debit','Services')")
 
+#Consigne n°1 : Supprimer toutes les lignes ayant au moins une cellule vide
+#On peut enfin supprimer toutes les lignes avec une cellule vide
+df = df.dropna()
+
+#Consigne n°4 : Ajouter une colonne id, auto-incrémenté
+df.insert(0, 'id', range(1, 1 + len(df)))
+
 #Consigne n°5 : Écrire le nouveau jeu de données dans un fichier result.csv
 df.to_csv('result.csv', index=False)
