92dfe181bf9c0f71acf61f67be7f7eae5a5dc0d3
Guillaume Pontanier <guillaume.pontanier@gmail.com>
2023-05-15 16:08:28 +0200

    Filtre des résultats pour la colonne

--- fichier.py
+++ fichier.py
@@ -8,5 +8,8 @@ df = pd.read_csv('test.csv')
 #On supprime tout d'abord les colonnes inutiles (et donc celles vides qui nous feraient supprimer tout le tableau à cause de leurs lignes vides pour la consigne n°1)
 df = df[['Period', 'Data_value', 'Series_title_2']]
 
+#Consigne n°3 : Filtrer les résultats en ne gardant que les résultats avec Credit, Debit et Services dans la colonne Series_title_2
+df = df.query("Series_title_2 in ('Credit','Debit','Services')")
+
 #Consigne n°5 : Écrire le nouveau jeu de données dans un fichier result.csv
 df.to_csv('result.csv', index=False)
